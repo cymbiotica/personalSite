14bf8dccadb84ef99dc5c17b207d53b7b142d5e4
cymbiotica <cymbioticasahon@gmail.com>
2018-05-19 20:48:58 -0400

    dash branch created

---
+++ pages/portfolio/Dash Lessons/Project 4/js/scripts.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    
+    // When eyes button is clicked, toggle laser class on brain
+$(".flash").click(function() {
+  $(".brain").toggleClass('laser');
+});
+
+// When color button is clicked...
+$(".color").click(function() {
+  // assign each named color a random number 0-255
+  //
+  var red = Math.floor(Math.random() * 255);
+  var green = Math.floor(Math.random() * 255);
+  var blue = Math.floor(Math.random() * 255);
+  
+  //Display the three values in an alert window
+  //alert(red + "," + green + "," + blue);
+  
+  //gen an rgba for the css
+  var randomRGBA = 'rgba('+red+','+green+','+blue+',1)';
+  
+  //testing the above
+  //alert(randomRGBA);
+  
+  $("body").css("background", randomRGBA);  
+});
+
+    $(".move").click(function(){
+        $("img").toggle();
+    });
+
+});
